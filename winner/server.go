417301410353ab4e6fcdb20d571c946dcb85b525
gn-t-k <gntk1995422@gmail.com>
2023-05-04 06:38:52 +0900

    wip

--- winner/server.go
+++ winner/server.go
@@ -3,10 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-// TODO: https://andmorefine.gitbook.io/learn-go-with-tests/build-an-application/http-server#nitesutowoku-1
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "20")
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
+}
+
+func GetPlayerScore(name string) string {
+	if name == "Pepper" {
+		return "20"
+	}
+
+	if name == "Floyd" {
+		return "10"
+	}
+
+	return ""
 }
